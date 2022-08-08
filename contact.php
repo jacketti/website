b68cbae4c2fcb224c2e2117243460fa5ae2c2343
Robert Jacketti <robertgorter@gmail.com>
2022-08-08 15:52:11 +0200

    new version

---
+++ contact.php
@@ -0,0 +1,61 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+  $subject = $_POST['subject'];
+?>
+
+
+<?php
+	$email_from = "info@robertgorter.com";
+
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n".
+                "With subject: $subject.\n".
+                "Here is the message:\n $message".
+?>
+
+<?php
+
+  $to = "info@robertgorter.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
+
+<?php
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+?>
